de216cdb76f36fe9b77b13007d0e8c0fcfb3779b
David Carle <david@numorning.com>
2018-11-02 16:54:16 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
+    <DBanner/>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DBanner from "./components/DBanner.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    DBanner
   }
 };
 </script>
---
+++ src/components/bundle.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context('.', false, /D[\w-]+.vue$/)
+
+requireComponent.keys().forEach(filename => {
+  // Get Component Config
+  const componentConfig = requireComponent(filename)
+
+  // Get PascalCase name of component
+  const componentName = upperFirst(camelCase(filename.replace(/^\.\//, '').replace(/\.\w+$/, '')))
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
+export default new Vue()
